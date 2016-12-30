4b2e6459152a30bc6535890ef4c4b4bfaa6fca99
George Torgovtsev <utorgo@gmail.com>
2016-12-30 23:51:55 +0300

    Домашнее задание добавил парметр $substArr в методы execute и query

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -13,8 +13,9 @@ abstract class Model
         $db = new \App\Db();
 
         return $db->query(
-            'SELECT * FROM ' . static::TABLE, //self::TABLE, //self::$table,
-            static::class //self::class //'App\Models\User'
+            'SELECT * FROM ' . static::TABLE .'WHERE id=:id', //self::TABLE, //self::$table,
+            static::class, //self::class //'App\Models\User'
+        	[':id' => 1]
         );
     }
 
