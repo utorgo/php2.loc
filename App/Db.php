07c1edcc672cfff4d41565a0e7e296c4a0c66b7c
George Torgovtsev <utorgo@gmail.com>
2016-12-30 18:20:42 +0300

    добавлен метод execute класса DB

--- App/Db.php
+++ App/Db.php
@@ -5,8 +5,18 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
+
     public function __construct()
     {
-        echo 'Hello, DB!!!';
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test','test','+1279484');
     }
+
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
+    }
+
 }
--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 
     $db = new \App\Db();
 
+    //$res = $db->execute('CREATE TABLE foo (id SERIAL)');
+
+    $res = $db->execute('SELECT * FROM persons');
+
+    var_dump($res);
+
