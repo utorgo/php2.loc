9df9fea50998c6c73a64b9950f08435000416112
George Torgovtsev <utorgo@gmail.com>
2017-01-12 15:25:59 +0300

    Добавил public static findById($id) в Model

---
+++ tests/findall.php
@@ -0,0 +1,15 @@
+<?php
+use App\Models\User;
+
+require __DIR__ . '\..\autoload.php';
+
+//$table = \App\Models\User::$table;
+$table = \App\Models\User::TABLE;
+
+echo $table . '</br>';
+
+$users = \App\Models\User::findAll();
+
+var_dump($users);
+
+
