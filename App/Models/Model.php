711ba9b395afd3bbeecda26f283bc429f687c30c
George Torgovtsev <utorgo@gmail.com>
2017-01-16 00:03:59 +0300

    исправил findById($id):  в запросе поменял ' на ".

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -13,11 +13,20 @@ abstract class Model
         $db = new \App\Db();
 
         return $db->query(
-            'SELECT * FROM ' . static::TABLE .'WHERE id=:id', //self::TABLE, //self::$table,
+            'SELECT * FROM ' . static::TABLE, //self::TABLE, //self::$table,
             static::class, //self::class //'App\Models\User'
         	[':id' => 1]
         );
     }
 
-    //abstract public function getName();
+    public static function findById($id='1')
+    {
+        $db = new \App\Db();
+
+        return $db->query(
+            "SELECT * FROM " . static::TABLE ." WHERE id=:id", //self::TABLE, //self::$table,
+            static::class, //self::class //'App\Models\User'
+            [':id' => $id]
+        );
+    }
 }
